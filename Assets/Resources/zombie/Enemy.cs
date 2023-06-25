10d60df7517aecc00621bd5e30e34d05bdf71cfb
Wosgu <100934950+Wosgu@users.noreply.github.com>
2023-06-25 18:28:12 +0300

    Enemy
    Научили зомби поворачиваться лицом туда, куда он идёт.

--- Assets/Resources/zombie/Enemy.cs
+++ Assets/Resources/zombie/Enemy.cs
@@ -7,13 +7,9 @@ public class Enemy : MonoBehaviour
     public GameObject[] point = new GameObject[14];
     private int action, rand = 0;
     public float speed = 1f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Vector3 eulerAngles;
+    private object quaternion;
 
-    // Update is called once per frame
     void Update()
     {
         if (action == 0)
@@ -25,17 +21,24 @@ public class Enemy : MonoBehaviour
                     point[i].transform.parent = null;
                 }
             }
+
             if (transform.position != point[rand].transform.position)
             {
-                transform.position = Vector3.MoveTowards(transform.position, point[rand].transform.position, speed = Time.deltaTime);
-                var direction = point[rand].transform.forward - transform.position;
-                var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-                transform.rotation = Quaternion.Euler(0, 0, angle);
+                Vector3 direction = point[rand].transform.position - transform.position;
+                transform.position = Vector3.MoveTowards(transform.position, point[rand].transform.position, speed * Time.deltaTime);
+                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+                eulerAngles = transform.eulerAngles;
+                eulerAngles.z = angle;
+                transform.eulerAngles = eulerAngles;
             }
             else
+            {
                 rand = Random.Range(0, 13);
+            }
         }
     }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         rand = Random.Range(0, 13);
