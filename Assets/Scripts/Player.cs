699440d716059cd2234bd49c2cf80c1b5f5ad035
Tamerlan319 <tamerlan.soslambekov@gmail.com>
2023-06-27 05:31:00 +0300

    Commit_Rel
    Переключение оружий у персонажа(логика ясна, недоработка в аниматоре, нужно допилить)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,6 +13,18 @@ public class Player : MonoBehaviour
     private Vector2 moveVelocity;
     public static int health = 100;
     public int curhlt;
+    public Animator anim;
+    public Gun gun1;
+
+
+    public AnimationClip atKnife, atAk, atDrob, idle;
+
+    public Sprite ak, drob, knife;
+    public SpriteRenderer spriteRenderer;
+
+    private int sostoyanie = 2;
+
+    public Transform player;
 
 
     public void TakeDamage(int damage)
@@ -31,11 +43,17 @@ public class Player : MonoBehaviour
         //curhlt = health;
         health = curhlt;
         rb = GetComponent<Rigidbody2D>();
+        anim = player.GetComponent<Animator>();
+        
+        //spriteRenderer = GetComponent<SpriteRenderer>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        //anim.SetBool("Attack", false);
+        //anim.SetBool("idleKnife", true);
         Vector3 moveVector = (Vector3.up * joystick.Horizontal + Vector3.left * joystick.Vertical);
         if (joystick.Horizontal != 0 || joystick.Vertical != 0)
         {
@@ -43,11 +61,57 @@ public class Player : MonoBehaviour
         }
         moveInput = new Vector2(joystick.Horizontal, joystick.Vertical);
         moveVelocity = moveInput.normalized * speed;
+        switch (sostoyanie)
+        {
+            case 0:
+                spriteRenderer.sprite = knife;
+                //anim.SetBool("AttackKnife", false);
+                //anim.SetBool("AttackAk", false);
+                //anim.SetBool("AttackDrob", false);
+                //anim.SetBool("IdleKnife", true);
+
+                gun1.gameObject.SetActive(false);
+                break;
+            case 1:
+
+                //anim.SetBool("AttackKnife", false);
+                //anim.SetBool("AttackAk", true);
+                //anim.SetBool("AttackDrob", false);
+                //anim.SetBool("IdleKnife", false);
+
+                gun1.gameObject.SetActive(true);
+                spriteRenderer.sprite = ak;
+                break;
+            case 2:
+                anim.SetBool("AttackKnife", false);
+                anim.SetBool("AttackAk", false);
+                anim.SetBool("AttackDrob", true);
+                anim.SetBool("IdleKnife", false);
+
+                
+                gun1.gameObject.SetActive(false);
+                spriteRenderer.sprite = drob;
+
+                break;
+            default: break;
+        }
 
     }
 
     private void FixedUpdate()
     {
         rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+    public void Gun1()
+    {
+        sostoyanie = 1;
+    }
+    public void Gun2()
+    {
+        sostoyanie = 2;
+    }
+    public void Knife()
+    {
+        sostoyanie = 0;
     }
 }
