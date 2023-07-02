7d04f24a66730fa8a3bfa43e943372d34d1f94d1
Wosgu <100934950+Wosgu@users.noreply.github.com>
2023-07-03 00:18:44 +0300

    End_3lvl
    Полностью загружены элементы интерфейса и зомби на 3 уровень, добавлена завершающая сцена.

---
+++ Assets/Scripts/GoSample.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GoSample : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+    public void onClick()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
